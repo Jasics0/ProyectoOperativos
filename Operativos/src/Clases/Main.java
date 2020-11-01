c6d527eefaa67d4d0707243f69a39718049ecf32
Javier <javierandrescharry@gmail.com>
2020-11-01 18:10:34 -0500

    asdsdas

--- Operativos/src/Clases/Main.java
+++ Operativos/src/Clases/Main.java
@@ -3,6 +3,7 @@ package Clases;
 public class Main {
 
     public static void main(String[] args) {
+        //Noseatrollperrito
         Cliente c = new Cliente();
         Servidor s = new Servidor();
         s.start();
