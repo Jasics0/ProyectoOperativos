918af4945b8d1931004aeb619264d104b3f13e6c
Javier <javierandrescharry@gmail.com>
2020-11-01 18:13:07 -0500

    xd

--- Operativos/src/Clases/Main.java
+++ Operativos/src/Clases/Main.java
@@ -3,6 +3,7 @@ package Clases;
 public class Main {
 
     public static void main(String[] args) {
+        //Noseatroll
         Cliente c = new Cliente();
         Servidor s = new Servidor();
         s.start();
