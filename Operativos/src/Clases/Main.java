606af0c032a74a77c565df34ac5cc990a4ed8c75
henrymartinez296 <martinezhenry296@gmail.com>
2020-11-01 18:13:52 -0500

    Sisa nea

--- Operativos/src/Clases/Main.java
+++ Operativos/src/Clases/Main.java
@@ -3,7 +3,7 @@ package Clases;
 public class Main {
 
     public static void main(String[] args) {
-        //Noseatroll
+        //Jaja troll tu
         Cliente c = new Cliente();
         Servidor s = new Servidor();
         s.start();
