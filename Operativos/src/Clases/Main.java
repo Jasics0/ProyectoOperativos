0e811b220e6cfa3359e808dfa13134c6decf9377
Javier <javierandrescharry@gmail.com>
2020-11-01 18:02:36 -0500

    xd

--- Operativos/src/Clases/Main.java
+++ Operativos/src/Clases/Main.java
@@ -1,13 +1,14 @@
 package Clases;
 
 public class Main {
-	
-	public static void main(String[] args) {
-		//Holisjuasjuas
-	Cliente c= new Cliente();
-	Servidor s= new Servidor();
+
+    public static void main(String[] args){
+    //Holisjuasjuas
+    //Henrygei
+    Cliente c = new Cliente();
+    Servidor s = new Servidor();
 	s.start();
 	c.start();
-	}
+}
 	
 }
