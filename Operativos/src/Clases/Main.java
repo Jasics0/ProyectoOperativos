4e6f0365704055ee47a313aaaf76c7854df15c5d
henrymartinez296 <martinezhenry296@gmail.com>
2020-11-01 18:09:05 -0500

    Estoy lol

--- Operativos/src/Clases/Main.java
+++ Operativos/src/Clases/Main.java
@@ -2,15 +2,11 @@ package Clases;
 
 public class Main {
 
-    public static void main(String[] args){
-    //Holisjuasjuas
-    //Henrygei
-        //Supapapa xddxdxdxd
-        
-    Cliente c = new Cliente();
-    Servidor s = new Servidor();
-	s.start();
-	c.start();
-}
-	
+    public static void main(String[] args) {
+        Cliente c = new Cliente();
+        Servidor s = new Servidor();
+        s.start();
+        c.start();
+    }
+
 }
